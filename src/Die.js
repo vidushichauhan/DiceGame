664680b82014044d282f9f8a0da1bd8119119a16
vidushichauhan <vidushic0@gmail.com>
2021-05-22 20:01:46 +0530

    added dice

---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Die extends Component{
+render(){
+    return(
+        <i className={`fas fa-dice-${this.props.face}`}/>
+    );
+}
+
+}
+
+export default Die;
