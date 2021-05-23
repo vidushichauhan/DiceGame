0820ff4d8f8b240d6d6cb68e0056f205dde557e2
vidushichauhan <vidushic0@gmail.com>
2021-05-23 11:01:59 +0530

    DiceGame Prepared

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 
-import Die from './Die';
+import RollDice from './RollDice';
 function App() {
   return (
     <div className="App">
-      <Die face='one'/>
-      <Die face='two'/>
-      <Die face='three'/>
-      <Die face='four'/>
-      <Die face='five'/>
-      <Die face='six'/>
+      <RollDice/>
+      
     </div>
   );
 }
--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import "./Die.css";
 class Die extends Component{
 render(){
     return(
-        <i className={`fas fa-dice-${this.props.face}`}/>
+        <i className={`Die fas fa-dice-${this.props.face}`}/>
     );
 }
 
