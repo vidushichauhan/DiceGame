d941c5d010b4e252946dcab226c0e9fd1a1b0516
vidushichauhan <vidushic0@gmail.com>
2021-05-23 11:39:32 +0530

    DiceGame Animation Added

--- src/RollDice.js
+++ src/RollDice.js
@@ -8,7 +8,7 @@ class RollDice extends Component{
     };
    constructor(props){
        super(props);
-       this.state={die1:'one',die2:'one'};
+       this.state={die1:'one',die2:'one',rolling:false};
        this.roll=this.roll.bind(this);
    }
    roll(){
@@ -20,16 +20,22 @@ class RollDice extends Component{
        this.props.sides[
         Math.floor(Math.random()*this.props.sides.length)
        ];
-       this.setState({die1:newDie1,die2:newDie2});
+       this.setState({die1:newDie1,die2:newDie2,rolling:true});
+
+       setTimeout(()=>{this.setState({rolling:false});       
+    },1000);
 }
 
     render(){
         return(
             <div className="RollDice"> 
             <div className="RollDice-container">
-                <Die face={this.state.die1}/>
-                <Die face={this.state.die2}/></div>
-                <button onClick={this.roll}>Roll Dice!!</button>
+                <Die face={this.state.die1} rolling={this.state.rolling}/>
+                <Die face={this.state.die2} rolling={this.state.rolling}/></div>
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                  {this.state.rolling ? 'Rolling':'RollDice'}
+
+                </button>
             </div>
 
             );
